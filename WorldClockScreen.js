3628a88d2f020c5034bc4040046a189752cd05fd
saleos97 <tai_nhan97@yahoo.de>
2019-01-06 15:28:56 +0700

    First commit

---
+++ WorldClockScreen.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+class WorldClockScreen extends React.Component {
+    render() {
+      return (
+        <View style={styles.container}>
+          <Text>WorldClock</Text>
+        </View>
+      );
+    }
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+  
+  export default WorldClockScreen;
